6397f5ff20d4b1cbd7f10ac91ed18e1c515353cd
dingan4109 <94068582+dingan4109@users.noreply.github.com>
2022-08-25 01:14:51 +0700

    update casestudy & bt ss4

--- casestudy/src/app/app.module.ts
+++ casestudy/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CustomerComponent } from './customer/customer.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FacilityComponent } from './facility/facility.component';
+import { EditFacilityComponent } from './edit-facility/edit-facility.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomerComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavbarComponent,
+    FacilityComponent,
+    EditFacilityComponent
   ],
   imports: [
     BrowserModule,
