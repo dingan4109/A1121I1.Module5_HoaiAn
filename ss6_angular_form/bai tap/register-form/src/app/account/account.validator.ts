15d4a21e553a8e93bcc042ff33eed08ad207eb01
dingan4109 <94068582+dingan4109@users.noreply.github.com>
2022-08-31 21:19:56 +0700

    ss6-bt1

---
+++ ss6_angular_form/bai tap/register-form/src/app/account/account.validator.ts
@@ -0,0 +1,7 @@
+import {AbstractControl, ValidationErrors} from '@angular/forms';
+
+export function comparePassword(control: AbstractControl): ValidationErrors | null {
+  const check = control.value;
+  return (check.pass === check.confirmPass) ? null : {passwordNotMatch: true};
+
+}
