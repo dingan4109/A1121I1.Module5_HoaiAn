5278a1a2788147c0df255a6ae0c673d3287e4dd4
dingan4109 <94068582+dingan4109@users.noreply.github.com>
2022-09-01 15:03:00 +0700

    update case study

--- casestudy/src/app/app.module.ts
+++ casestudy/src/app/app.module.ts
@@ -3,24 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FacilityComponent } from './facility/facility.component';
 import { EditFacilityComponent } from './edit-facility/edit-facility.component';
 import { CreateNewFacilityComponent } from './create-new-facility/create-new-facility.component';
 import { CustomerListComponent } from './customer-list/customer-list.component';
+import { EditCustomerComponent } from './edit-customer/edit-customer.component';
+import { CreateCustomerComponent } from './create-customer/create-customer.component';
+import { ContractListComponent } from './contract-list/contract-list.component';
+import { CustomerUseFacilityComponent } from './customer-use-facility/customer-use-facility.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
     FooterComponent,
     NavbarComponent,
     FacilityComponent,
     EditFacilityComponent,
     CreateNewFacilityComponent,
-    CustomerListComponent
+    CustomerListComponent,
+    EditCustomerComponent,
+    CreateCustomerComponent,
+    ContractListComponent,
+    CustomerUseFacilityComponent
   ],
   imports: [
     BrowserModule,
